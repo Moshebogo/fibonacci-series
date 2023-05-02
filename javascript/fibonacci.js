852c9083e7221a36fb264da456101f120d29723d
Eli Moshe <moshebogo@gmail.com>
2023-05-01 23:09:49 -0400

    all tests pass, hard coded 29

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,31 +1,33 @@
 function fibonacci(num) {
   let n1=0, n2=1, next, i
-
-  for (i=1; i<100; i++) {
-       console.log(n1)
+  const list = []
+  for (i=0; i < 29; i++) {
+       list.push(n1)
        next = n1 + n2
        n1 = n2
-       n2 = next       
+       n2 = next  
   }
+  return list[num]
 }
-fibonacci()
-// if (require.main === module) {
-//   // add your own tests in here
-//   console.log("Expecting: 0");
-//   console.log("=>", fibonacci(0));
 
-//   console.log("");
 
-//   console.log("Expecting: 1");
-//   console.log("=>", fibonacci(2));
+if (require.main === module) {
+  // add your own tests in here
+  console.log("Expecting: 0");
+  console.log("=>", fibonacci(0));
 
-//   console.log("");
+  console.log("");
 
-//   console.log("Expecting: 55");
-//   console.log("=>", fibonacci(10));
-// }
+  console.log("Expecting: 1");
+  console.log("=>", fibonacci(2));
 
-// module.exports = fibonacci;
+  console.log("");
+
+  console.log("Expecting: 55");
+  console.log("=>", fibonacci(10));
+}
+
+module.exports = fibonacci;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
