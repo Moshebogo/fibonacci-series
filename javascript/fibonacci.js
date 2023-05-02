54e11b82ddc22bea706706b193215e7f213feed8
Eli Moshe <moshebogo@gmail.com>
2023-05-01 22:31:02 -0400

    generating the numbers

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,24 +1,31 @@
 function fibonacci(num) {
-  // type your code here
+  let n1=0, n2=1, next, i
+
+  for (i=1; i<100; i++) {
+       console.log(n1)
+       next = n1 + n2
+       n1 = n2
+       n2 = next       
+  }
 }
+fibonacci()
+// if (require.main === module) {
+//   // add your own tests in here
+//   console.log("Expecting: 0");
+//   console.log("=>", fibonacci(0));
 
-if (require.main === module) {
-  // add your own tests in here
-  console.log("Expecting: 0");
-  console.log("=>", fibonacci(0));
+//   console.log("");
 
-  console.log("");
+//   console.log("Expecting: 1");
+//   console.log("=>", fibonacci(2));
 
-  console.log("Expecting: 1");
-  console.log("=>", fibonacci(2));
+//   console.log("");
 
-  console.log("");
+//   console.log("Expecting: 55");
+//   console.log("=>", fibonacci(10));
+// }
 
-  console.log("Expecting: 55");
-  console.log("=>", fibonacci(10));
-}
-
-module.exports = fibonacci;
+// module.exports = fibonacci;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
